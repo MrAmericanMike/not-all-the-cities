b737f382f98cd637e13cbe92e82b0926f7f9105b
MrAmericanMike <mramericanmike@gmail.com>
2024-10-08 19:47:47 -0300

    Rollup Test Updates

---
+++ rollup.config.mjs
@@ -0,0 +1,24 @@
+import typescript from "@rollup/plugin-typescript";
+import deletePlugin from "rollup-plugin-delete";
+
+export default {
+	input: "src/main.ts",
+	plugins: [deletePlugin({ targets: "dist/*" }), typescript()],
+	output: [
+		{
+			file: "dist/main.cjs",
+			format: "cjs",
+			exports: "auto"
+		},
+		{
+			file: "dist/main.mjs",
+			format: "es"
+		},
+		{
+			file: "dist/main.umd.js",
+			format: "umd",
+			name: "NotAllTheCities",
+			exports: "auto"
+		}
+	]
+};
